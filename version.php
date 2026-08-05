3337c2cad4743bafc3b3a38019a98534a23847f6
Sepolino <viniszsx505@gmail.com>
2026-08-05 02:23:14 -0300

    Update version number to 2026080509 in version.php

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'local_feedbackdashboard';
-$plugin->version   = 2026080508;
+$plugin->version   = 2026080509;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '1.0.0-alpha';
