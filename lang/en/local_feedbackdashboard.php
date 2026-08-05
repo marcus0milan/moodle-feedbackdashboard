b4404d8ed11b7d8b7299b427eb26372d07153111
Sepolino <viniszsx505@gmail.com>
2026-08-05 00:28:34 -0300

    Added English language strings for feedback dashboard..

---
+++ lang/en/local_feedbackdashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Feedback dashboard';
+$string['dashboard'] = 'Dashboard';
+$string['dashboardtitle'] = 'Feedback dashboard';
+$string['feedbackname'] = 'Feedback';
+$string['coursename'] = 'Course';
+$string['responsecount'] = 'Submitted responses';
+$string['questionscount'] = 'Questions';
+$string['anonymousfeedback'] = 'Anonymous feedback';
+$string['identifiedfeedback'] = 'Identified feedback';
+$string['pluginnotavailable'] = 'The Dashboard is available only for Feedback activities.';
