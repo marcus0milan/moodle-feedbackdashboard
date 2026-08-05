45a903a55b4501b807b62314881c3b5ffb782f0e
Sepolino <viniszsx505@gmail.com>
2026-08-05 01:07:05 -0300

    updated version

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_feedbackdashboard';
-$plugin->version   = 2026080502;
+$plugin->version   = 2026080503;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '1.0.0-alpha';
