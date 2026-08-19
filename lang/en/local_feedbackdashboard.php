fa8bef5791d0411bb97e31251ef2900c24cb4465
Sepolino <viniszsx505@gmail.com>
2026-08-19 20:10:53 -0300

    Add English and Brazilian Portuguese language strings for Feedback Dashboard

---
+++ lang/en/local_feedbackdashboard.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * English language strings for Feedback Dashboard.
+ *
+ * @package    local_feedbackdashboard
+ * @copyright  2026 Marcus Vinícius Milan da Silva
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'NPS Dashboard';
+$string['opendashboard'] = 'Open NPS Dashboard';
+$string['admindashboard'] = 'NPS Dashboard';
+$string['admindashboardheading'] = 'NPS Dashboard overview';
+$string['admindashboarddescription'] = 'Compare Moodle Feedback activities and open the detailed NPS report for each activity.';
+$string['searchfeedbacks'] = 'Search by course or feedback name';
+$string['search'] = 'Search';
+$string['clear'] = 'Clear';
+$string['course'] = 'Course';
+$string['feedback'] = 'Feedback activity';
+$string['responses'] = 'Responses';
+$string['validnpsresponses'] = 'Valid NPS ratings';
+$string['nps'] = 'NPS';
+$string['promoters'] = 'Promoters';
+$string['passives'] = 'Passives';
+$string['detractors'] = 'Detractors';
+$string['lastresponse'] = 'Last response';
+$string['actions'] = 'Actions';
+$string['openactivity'] = 'Open activity';
+$string['openanalysis'] = 'Open native analysis';
+$string['nonpsquestion'] = 'No 0–10 NPS question detected';
+$string['noresponses'] = 'No responses';
+$string['nofeedbacks'] = 'No Feedback activities were found for the current filter.';
+$string['totalsurveys'] = 'Feedback activities';
+$string['totalresponses'] = 'Responses';
+$string['globalnps'] = 'Combined NPS';
+$string['surveyswithnps'] = 'Activities with NPS';
+$string['privacy:metadata'] = 'The Feedback Dashboard plugin does not store personal data. It reads data already held by Moodle Feedback and displays it to authorised users.';
+$string['feedbackdashboard:view'] = 'View an NPS dashboard for a Feedback activity';
+$string['feedbackdashboard:viewall'] = 'View the site-wide NPS dashboard';
---
+++ lang/pt_br/local_feedbackdashboard.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Brazilian Portuguese language strings for Feedback Dashboard.
+ *
+ * @package    local_feedbackdashboard
+ * @copyright  2026 Marcus Vinícius Milan da Silva
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Dashboard NPS';
+$string['opendashboard'] = 'Abrir Dashboard NPS';
+$string['admindashboard'] = 'Dashboard NPS';
+$string['admindashboardheading'] = 'Visão geral do NPS';
+$string['admindashboarddescription'] = 'Compare as atividades Pesquisa do Moodle e acesse o relatório detalhado de NPS de cada atividade.';
+$string['searchfeedbacks'] = 'Pesquisar por curso ou nome da pesquisa';
+$string['search'] = 'Pesquisar';
+$string['clear'] = 'Limpar';
+$string['course'] = 'Curso';
+$string['feedback'] = 'Atividade Pesquisa';
+$string['responses'] = 'Respostas';
+$string['validnpsresponses'] = 'Notas NPS válidas';
+$string['nps'] = 'NPS';
+$string['promoters'] = 'Promotores';
+$string['passives'] = 'Neutros';
+$string['detractors'] = 'Detratores';
+$string['lastresponse'] = 'Última resposta';
+$string['actions'] = 'Ações';
+$string['openactivity'] = 'Abrir atividade';
+$string['openanalysis'] = 'Abrir análise nativa';
+$string['nonpsquestion'] = 'Nenhuma pergunta NPS de 0 a 10 detectada';
+$string['noresponses'] = 'Sem respostas';
+$string['nofeedbacks'] = 'Nenhuma atividade Pesquisa foi encontrada para o filtro atual.';
+$string['totalsurveys'] = 'Pesquisas';
+$string['totalresponses'] = 'Respostas';
+$string['globalnps'] = 'NPS combinado';
+$string['surveyswithnps'] = 'Pesquisas com NPS';
+$string['privacy:metadata'] = 'O plugin Dashboard NPS não armazena dados pessoais. Ele apenas lê dados já mantidos pela atividade Pesquisa do Moodle e os exibe para usuários autorizados.';
+$string['feedbackdashboard:view'] = 'Visualizar o dashboard NPS de uma atividade Pesquisa';
+$string['feedbackdashboard:viewall'] = 'Visualizar o dashboard geral de NPS do site';
