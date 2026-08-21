00806de7023eb6c2243798110f277a28fbfe4a0b
marcus0milan <viniszsx505@gmail.com>
2026-08-21 15:18:52 -0300

    Update plugin version to 1.1.4-beta and increment version number

--- version.php
+++ version.php
@@ -18,8 +18,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'local_feedbackdashboard';
-$plugin->version   = 2026081903;
+$plugin->version   = 2026081904;
 $plugin->requires  = 2024042200;
 $plugin->supported = [404, 405];
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.1.3-beta';
+$plugin->release   = '1.1.4-beta';
