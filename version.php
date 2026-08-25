f6982b0c476738e072a03e37cfd67257fb0423fc
marcus0milan <viniszsx505@gmail.com>
2026-08-25 16:04:06 -0300

    Bump plugin version to 2026081915

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'local_feedbackdashboard';
-$plugin->version   = 2026081914;
+$plugin->version   = 2026081915;
 $plugin->requires  = 2024042200;
 $plugin->supported = [404, 405];
 $plugin->maturity  = MATURITY_BETA;
