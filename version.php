2c171610ad7a6e3b5921f6ae539a97823ba556a8
Sepolino <viniszsx505@gmail.com>
2026-08-27 19:48:06 -0300

    Bump plugin version to 2026082607

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'local_feedbackdashboard';
-$plugin->version   = 2026082606;
+$plugin->version   = 2026082607;
 $plugin->requires  = 2024042200;
 $plugin->supported = [404, 405];
 $plugin->maturity  = MATURITY_BETA;
